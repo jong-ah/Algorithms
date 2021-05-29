fb6a587b32ae26122374b6b9745818236cc8b738
Park Jong Ah <jonga6431@gmail.com>
2021-05-29 21:44:00 +0900

    프로그래머스 레벨1 행렬의 덧셈

---
+++ algorithm15.js
@@ -0,0 +1,19 @@
+function solution(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr1[i].length; j++) {
+      arr1[i][j] += arr2[i][j];
+    }
+  }
+  console.log(arr1);
+  return arr1;
+}
+
+let arr1 = [
+  [1, 2],
+  [2, 3],
+];
+let arr2 = [
+  [3, 4],
+  [5, 6],
+];
+solution(arr1, arr2);
