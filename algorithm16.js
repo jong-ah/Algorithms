c833aa0ed7b17eb32f746f571060f340a993903d
Park Jong Ah <jonga6431@gmail.com>
2021-05-29 22:09:07 +0900

    프로그래머스 레벨1 직사각형 별찍기

---
+++ algorithm16.js
@@ -0,0 +1,18 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+  const n = data.split(' ');
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  let result = '';
+  let answer = '';
+
+  for (let i = 0; i < a; i++) {
+    result = result + '*';
+  }
+
+  for (let i = 0; i < b; i++) {
+    answer = result + `\n` + answer;
+  }
+  
+  console.log(answer);
+});
