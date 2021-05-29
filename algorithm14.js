6ce4cdf3ff7599242fdf9ac2d747821bcbf27505
Park Jong Ah <jonga6431@gmail.com>
2021-05-29 20:37:38 +0900

    프로그래머스 레벨1 최대공약수와 최소공배수

--- algorithm14.js
+++ algorithm14.js
@@ -1,26 +1,46 @@
-function solution(n, m) {
+// 유클리드 호제법 -> 큰수%작은수, 값이 0일 때 리턴
+function solution1(n, m) {
   const arr = [n, m];
-  const answer = [];
+
+  arr.sort((a, b) => a - b);
+
+  const 최대공약수 = (arr) => {
+    return arr.reduce((a, b) => {
+      if (b % a === 0) return a;
+      return 최대공약수([b % a, a]);
+    });
+  };
+
+  const 최소공배수 = (arr) => {
+    return arr.reduce((a, b) => (a * b) / 최대공약수(arr));
+  };
+
+  console.log('최대공약수', 최대공약수(arr), ',최소공배수', 최소공배수(arr));
+  return [최대공약수(arr), 최소공배수(arr)];
+}
+
+// 유클리드 호제법 변형 -> 작은수%큰수, 큰수가 0일 때 리턴
+function solution2(n, m) {
+  const arr = [n, m];
 
   arr.sort((a, b) => a - b);
 
   const 최대공약수 = (arr) => {
     return arr.reduce((a, b) => {
       if (b === 0) return a;
-      if (a % b === 1) return 1;
       return 최대공약수([b, a % b]);
     });
   };
-  answer.push(최대공약수(arr));
 
   const 최소공배수 = (arr) => {
     return arr.reduce((a, b) => (a * b) / 최대공약수(arr));
   };
-  answer.push(최소공배수(arr));
 
-  console.log('최대공약수', answer[0], ',최소공배수', answer[1]);
-  return answer;
+  console.log('최대공약수', 최대공약수(arr), ',최소공배수', 최소공배수(arr));
+  return [최대공약수(arr), 최소공배수(arr)];
 }
+
 let n = 2;
 let m = 5;
-solution(n, m);
+solution1(n, m);
+solution2(n, m);
