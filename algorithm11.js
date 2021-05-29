b93ec2369ef37cd6554c25443286defdbe31b17a
Park Jong Ah <jonga6431@gmail.com>
2021-05-29 09:25:00 +0900

    프로그래머스 레벨1 같은 숫자는 싫어

---
+++ algorithm11.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+  var answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      continue;
+    } else {
+      answer.push(arr[i]);
+    }
+  }
+  console.log(answer);
+  return answer;
+}
+
+let arr = [1, 1, 3, 3, 0, 1, 1];
+solution(arr);
