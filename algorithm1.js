4513166a5bbf83b053e027820aa8984092a5067d
Park Jong Ah <jonga6431@gmail.com>
2021-06-07 00:39:46 +0900

    프로그래머스 레벨1 체육복

--- algorithm1.js
+++ algorithm1.js
@@ -1,30 +1,33 @@
 function solution(n, lost, reserve) {
-  var answer = 0;
-    
-  // lost가 기준이다.
-  // 우선 같은 번호은 없앤다.
-  for ( let i = 0; i < lost.length; i++ ) {
-    if ( reserve.indexOf(lost[i]) !== -1 ) {
-      lost.splice(i, 1);
-      reserve.splice(reserve.indexOf(lost[i]), 1);
-    }
-  }
-    
-  // sort로 순서로 맞춰준다.
-  lost.sort((a,b)=> a-b);
-  reserve.sort((a,b)=> a-b);
-  
-  // reserve의 -1과 +1 중에선 indexOf(lost) 값으로 
-  // 동일한 게 있는지 확인한다.
-  for ( let i = 0; i < reserve.length; i++ ) {
-    for ( let j = 0; j < lost.length; j++ ) {
-      if ( [reserve[i]-1, reserve[i]+1].indexOf(lost[j]) !== -1 ) {
-        lost.splice(j, 1);
-        reserve.splice(i, 1);
+  if (lost.length === 0) return n;
+  if (reserve.length === 0) return n - lost.length;
+
+  let filterLost = lost.filter((el) => {
+    if (reserve.includes(el) === true) {
+      reserve.splice(reserve.indexOf(el), 1);
+      return false;
+    } else return true;
+  });
+
+  let mapReserve = reserve.sort((a, b) => a - b).map((el) => [el - 1, el + 1]);
+  let resultLost = filterLost
+    .sort((a, b) => a - b)
+    .filter((el) => {
+      for (let i = 0; i < mapReserve.length; i++) {
+        console.log(mapReserve[i], el);
+        if (mapReserve[i].includes(el)) {
+          mapReserve.splice(i, 1);
+          return false;
+        }
       }
-    }
-  }
-    
-  answer = n - lost.length;
-  return answer;
+      return true;
+    });
+
+  return n - resultLost.length;
 }
+
+let n = 3;
+let lost = [3];
+let reserve = [1];
+let output = solution(n, lost, reserve);
+console.log(output); 
